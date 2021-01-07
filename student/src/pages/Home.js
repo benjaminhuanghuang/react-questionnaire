30008588c7028ce3394e2982047a18081dedebb9
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-01-06 21:29:51 -0800

    update

--- student/src/pages/Home.js
+++ student/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h2>Welcome, Please log in.</h2>
+        <h2>Welcome to panda questionnar. Please log in before answer quesitons.</h2>
     </div>
   )
 }
